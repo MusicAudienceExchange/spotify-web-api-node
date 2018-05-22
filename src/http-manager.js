c3d49a87c076d33d6be103d2070a4d9faa02075c
Leo Antelyes <leo@settheset.com>
2018-05-22 14:46:15 -0700

    Revert "rearange code"
    This reverts commit 464837c74ff39b0bc8a193a1ed056cd6c893d752.

--- src/http-manager.js
+++ src/http-manager.js
@@ -6,6 +6,11 @@ var superagent = require('superagent'),
 var HttpManager = {};
 
 
+/* Simple delay function using promises */
+const _timeout = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 /* Create superagent options from the base request */
 var _getParametersFromRequest = function(request) {
   var options = {};
@@ -89,25 +94,19 @@ HttpManager._makeRequest = function(method, options, uri, callback) {
 
   req.end(function(err, response) {
 
+    if (response.statusCode === 429) {
 
+      setTimeout(() => {
+        return HttpManager._makeRequest(method, options, uri, callback);
+      }, response.headers['retry-after'] * 1000)
 
-    if (err) {
+    } else if (err) {
 
-      if (response.statusCode === 429) {
-
-        if(response.headers && response.headers['retry-after']) {
-          setTimeout(() => {
-            return HttpManager._makeRequest(method, options, uri, callback);
-          }, response.headers['retry-after'] * 1000)
-        }
-      } else {
-        
-        var errorObject = _getErrorObject('Request error', {
-          error: err,
-          headers: (response != null && typeof response != 'undefined' && typeof response.headers != 'undefined') ? response.headers : null
-        });
-        return callback(errorObject);
-      }
+      var errorObject = _getErrorObject('Request error', {
+        error: err,
+        headers: (response != null && typeof response != 'undefined' && typeof response.headers != 'undefined') ? response.headers : null
+      });
+      return callback(errorObject);
 
     } else {
 
